6a400f5d6aa90142d518f8f72aca0b60f904f095
Sébastien Lerique <seblerique@wanadoo.fr>
2013-01-10 13:31:22 +0100

    Forgot to add the file in previous commit

---
+++ yelandur/settings_test.py
@@ -0,0 +1,15 @@
+# API URL
+API_URL = '/api'
+API_VERSION_URL = API_URL + '/v1'
+
+# Miscellaneous options
+SECRET_KEY = 'test key'
+DEBUG = True
+TESTING = True
+HOST = '0.0.0.0'
+
+# Upload options
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+# MongoDB options
+MONGODB_DB = 'yelandur_test'
