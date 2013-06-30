83d3bbd366441fe8c8bd7943f7f15510373a27b0
Sébastien Lerique <seblerique@wanadoo.fr>
2013-06-30 19:14:26 +0300

    Start adapting helper tests

--- yelandur/test_helpers.py
+++ yelandur/test_helpers.py
@@ -1,106 +1,106 @@
 # -*- coding: utf-8 -*-
 
-#import unittest
-#import re
-#from functools import partial
-#from datetime import datetime
-#from types import MethodType
+import unittest
+import re
+from functools import partial
+from datetime import datetime
+from types import MethodType
 
-#import ecdsa
-#import jws
-#from ecdsa.util import sigencode_der, sigdecode_string
-#from jws.utils import base64url_decode
-#from mongoengine import Document, StringField
+import ecdsa
+import jws
+from ecdsa.util import sigencode_der, sigdecode_string
+from jws.utils import base64url_decode
+from mongoengine import Document, StringField
 
-#from . import create_app, models, helpers
-
-
-#class HashTestCase(unittest.TestCase):
-
-    #def test_hexregex(self):
-        ## Two example cases
-        #self.assertTrue(re.search(helpers.hexregex, '345abcf'),
-                        #'bad hexregex')
-        #self.assertFalse(re.search(helpers.hexregex, '345abdg'),
-                         #'bad hexregex')
-
-    #def test_md5hex(self):
-        ## One example case
-        #self.assertEqual(helpers.md5hex('test'),
-                         #'098f6bcd4621d373cade4e832627b4f6',
-                         #'bad md5 hex digest')
-
-    #def test_sha256hex(self):
-        ## One example case
-        #self.assertEqual(helpers.sha256hex('test'),
-                         #('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15'
-                          #'d6c15b0f00a08'),
-                         #'bad sha256 hex digest')
-
-    #def test_random_md5hex(self):
-        #r1 = helpers.random_md5hex()
-        #r2 = helpers.random_md5hex()
-
-        ## Make sure hexregex is good
-        #self.test_hexregex()
-
-        ## r1 is a 32 characters-long hex hash
-        #self.assertEqual(len(r1), 32, 'bad md5hex length')
-        #self.assertTrue(re.search(helpers.hexregex, r1),
-                        #'bad md5hex format')
-
-        ## r1 and r2 are different
-        #self.assertFalse(r1 == r2, 'constant md5hex')
-
-    #def test_random_sha256hex(self):
-        #r1 = helpers.random_sha256hex()
-        #r2 = helpers.random_sha256hex()
-
-        ## Make sure hexregex is good
-        #self.test_hexregex()
-
-        ## r1 is a 64 characters-long hex hash
-        #self.assertEqual(len(r1), 64, 'bad sha256hex length')
-        #self.assertTrue(re.search(helpers.hexregex, r1),
-                        #'bad sha256hex format')
-
-        ## r1 and r2 are different
-        #self.assertFalse(r1 == r2, 'constant sha256hex')
-
-    #def test_build_gravatar_id(self):
-        ## One example case
-        #self.assertEqual(helpers.build_gravatar_id('johndoe@example.com'),
-                         #'fd876f8cd6a58277fc664d47ea10ad19',
-                         #'bad gravatar id')
+from . import create_app, models, helpers
 
 
-#class SigConversionTestCase(unittest.TestCase):
+class HashTestCase(unittest.TestCase):
 
-    #def test_sig_der_to_string(self):
-        ## One runtime generated example case
-        #sk = ecdsa.SigningKey.generate(curve=ecdsa.curves.NIST256p)
-        #order = sk.curve.order
-        #jheader = '{"alg": "ES256"}'
-        #jpayload = '{"test": "test"}'
-        #sig_string_b64 = jws.sign(jheader, jpayload, sk, is_json=True)
-        #sig_string = base64url_decode(sig_string_b64)
-        #r, s = sigdecode_string(sig_string, order)
-        #sig_der = sigencode_der(r, s, order)
-        #self.assertEqual(helpers.sig_der_to_string(sig_der, order), sig_string,
-                         #'bad der_to_string signature')
+    def test_hexregex(self):
+        # Two example cases
+        self.assertTrue(re.search(helpers.hexregex, '345abcf'),
+                        'bad hexregex')
+        self.assertFalse(re.search(helpers.hexregex, '345abdg'),
+                         'bad hexregex')
 
-        ## One static example case
-        #sig2_string = ('k\xfc\xf2(\x88\x13I\x87\xc0\xf7%\x90\x16PD\xeb\xace'
-                       #'\x89\x8f\r\xbe^u\xc6\x19\x19\xb8\xe1=\xb5\xc1\x1bT'
-                       #'\xc6\xa9\xea\xcdC\xa5\xaf\xd7~\xddB\x14mmBw\x8b\xcc'
-                       #'\xecS\xaa\x03\xfal\xe02\xf6\xcb \xc3')
-        #sig2_der = ('0D\x02 k\xfc\xf2(\x88\x13I\x87\xc0\xf7%\x90\x16PD\xeb'
-                    #'\xace\x89\x8f\r\xbe^u\xc6\x19\x19\xb8\xe1=\xb5\xc1\x02 '
-                    #'\x1bT\xc6\xa9\xea\xcdC\xa5\xaf\xd7~\xddB\x14mmBw\x8b\xcc'
-                    #'\xecS\xaa\x03\xfal\xe02\xf6\xcb \xc3')
-        #order = 115792089210356248762697446949407573529996955224135760342422259061068512044369L
-        #self.assertEqual(helpers.sig_der_to_string(sig2_der, order),
-                         #sig2_string, 'bad der_to_string signature')
+    def test_md5hex(self):
+        # One example case
+        self.assertEqual(helpers.md5hex('test'),
+                         '098f6bcd4621d373cade4e832627b4f6',
+                         'bad md5 hex digest')
+
+    def test_sha256hex(self):
+        # One example case
+        self.assertEqual(helpers.sha256hex('test'),
+                         ('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15'
+                          'd6c15b0f00a08'),
+                         'bad sha256 hex digest')
+
+    def test_random_md5hex(self):
+        r1 = helpers.random_md5hex()
+        r2 = helpers.random_md5hex()
+
+        # Make sure hexregex is good
+        self.test_hexregex()
+
+        # r1 is a 32 characters-long hex hash
+        self.assertEqual(len(r1), 32, 'bad md5hex length')
+        self.assertTrue(re.search(helpers.hexregex, r1),
+                        'bad md5hex format')
+
+        # r1 and r2 are different
+        self.assertFalse(r1 == r2, 'constant md5hex')
+
+    def test_random_sha256hex(self):
+        r1 = helpers.random_sha256hex()
+        r2 = helpers.random_sha256hex()
+
+        # Make sure hexregex is good
+        self.test_hexregex()
+
+        # r1 is a 64 characters-long hex hash
+        self.assertEqual(len(r1), 64, 'bad sha256hex length')
+        self.assertTrue(re.search(helpers.hexregex, r1),
+                        'bad sha256hex format')
+
+        # r1 and r2 are different
+        self.assertFalse(r1 == r2, 'constant sha256hex')
+
+    def test_build_gravatar_id(self):
+        # One example case
+        self.assertEqual(helpers.build_gravatar_id('johndoe@example.com'),
+                         'fd876f8cd6a58277fc664d47ea10ad19',
+                         'bad gravatar id')
+
+
+class SigConversionTestCase(unittest.TestCase):
+
+    def test_sig_der_to_string(self):
+        # One runtime generated example case
+        sk = ecdsa.SigningKey.generate(curve=ecdsa.curves.NIST256p)
+        order = sk.curve.order
+        jheader = '{"alg": "ES256"}'
+        jpayload = '{"test": "test"}'
+        sig_string_b64 = jws.sign(jheader, jpayload, sk, is_json=True)
+        sig_string = base64url_decode(sig_string_b64)
+        r, s = sigdecode_string(sig_string, order)
+        sig_der = sigencode_der(r, s, order)
+        self.assertEqual(helpers.sig_der_to_string(sig_der, order), sig_string,
+                         'bad der_to_string signature')
+
+        # One static example case
+        sig2_string = ('k\xfc\xf2(\x88\x13I\x87\xc0\xf7%\x90\x16PD\xeb\xace'
+                       '\x89\x8f\r\xbe^u\xc6\x19\x19\xb8\xe1=\xb5\xc1\x1bT'
+                       '\xc6\xa9\xea\xcdC\xa5\xaf\xd7~\xddB\x14mmBw\x8b\xcc'
+                       '\xecS\xaa\x03\xfal\xe02\xf6\xcb \xc3')
+        sig2_der = ('0D\x02 k\xfc\xf2(\x88\x13I\x87\xc0\xf7%\x90\x16PD\xeb'
+                    '\xace\x89\x8f\r\xbe^u\xc6\x19\x19\xb8\xe1=\xb5\xc1\x02 '
+                    '\x1bT\xc6\xa9\xea\xcdC\xa5\xaf\xd7~\xddB\x14mmBw\x8b\xcc'
+                    '\xecS\xaa\x03\xfal\xe02\xf6\xcb \xc3')
+        order = 115792089210356248762697446949407573529996955224135760342422259061068512044369L
+        self.assertEqual(helpers.sig_der_to_string(sig2_der, order),
+                         sig2_string, 'bad der_to_string signature')
 
 
 #class WipeDatabaseTestCase(unittest.TestCase):
