0a56e7710ecd735eca2f5e69c002abdb98dd9070
Sébastien Lerique <seblerique@wanadoo.fr>
2013-07-30 15:47:31 +0200

    Clean up old code from old blueprint moved to exps

--- yelandur/users.py
+++ yelandur/users.py
@@ -8,8 +8,7 @@ from mongoengine import NotUniqueError, ValidationError
 from mongoengine.queryset import DoesNotExist
 
 from .cors import cors
-from .models import User, UserIdSetError  # , Exp
-#from .helpers import jsonify
+from .models import User, UserIdSetError
 
 
 # Create the actual blueprint
@@ -89,93 +88,6 @@ class UserView(MethodView):
 
 
 users.add_url_rule('/<user_id>', view_func=UserView.as_view('user'))
-
-
-#class ExpsView(MethodView):
-
-    #@cors()
-    #def get(self, login):
-        #u = User.objects.get(login=login)
-        #exps = Exp.objects(owner=u)
-
-        #if current_user.is_authenticated() and current_user == u:
-            #return jsonify(exps.to_jsonable_private())
-        #else:
-            #return jsonify(exps.to_jsonable())
-
-    #@login_required
-    #@cors()
-    #def post(self, login):
-        #u = User.objects.get(login=login)
-
-        #if current_user == u:
-            #name = request.json.get('name') or request.json.get('name_claim')
-            #description = request.json.get('description')
-            ## Errors generated here are caught by errorhandlers, see below
-            #e = Exp.create(name, u, description)
-            #e.save()
-            #return jsonify(e.to_jsonable_private()), 201
-        #else:
-            ## User not authorized
-            #abort(403)
-
-    #@cors()
-    #def options(self, login):
-        #pass
-
-
-#users.add_url_rule('/<login>/exps/', view_func=ExpsView.as_view('exps'))
-
-
-#class ExpView(MethodView):
-
-    #@cors()
-    #def get(self, login, name):
-        #u = User.objects.get(login=login)
-        #e = Exp.objects(owner=u).get(name=name)
-
-        #if (current_user.is_authenticated() and
-                #(current_user == e.owner or current_user in e.collaborators)):
-            #return jsonify(e.to_jsonable_private())
-        #else:
-            #return jsonify(e.to_jsonable())
-
-    #@cors()
-    #@login_required
-    #def put(self, login, name):
-        #u = User.objects.get(login=login)
-        #e = Exp.objects(owner=u).get(name=name)
-
-        #if current_user == e.owner or current_user in e.collaborators:
-            #if name != request.json['name']:
-                #abort(400)
-            #e.description = request.json['description']
-            #e.save()
-            #return jsonify(e.to_jsonable_private())
-        #else:
-            ## User not authorized
-            #abort(403)
-
-    #@cors()
-    #def options(self, login, name):
-        #pass
-
-
-#users.add_url_rule('/<login>/exps/<name>', view_func=ExpView.as_view('exp'))
-
-
-#@users.route('/<login>/exps/<name>/results/')
-#@cors()
-#@login_required
-#def results(login, name):
-    ## No POST method here since results are added by devices only
-    #u = User.objects.get(login=login)
-    #e = Exp.objects(owner=u).get(name=name)
-
-    #if current_user == e.owner or current_user in e.collaborators:
-        #return jsonify(e.to_jsonable_private('results'))
-    #else:
-        #abort(403)
 
 
 @users.errorhandler(ValidationError)
