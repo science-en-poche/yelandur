c19d6ee54966df0d5ba5883d88af5f5c801b4a94
Sébastien Lerique <seblerique@wanadoo.fr>
2013-08-30 17:23:04 -0300

    Adapt dev settings

--- yelandur/settings_dev.py
+++ yelandur/settings_dev.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 # Miscellaneous options
-SECRET_KEY = 'development key'
+if 'FLASK_SECRET_KEY_DEV' in os.environ:
+    SECRET_KEY = os.environ['FLASK_SECRET_KEY_DEV']
+else:
+    SECRET_KEY = 'development key'
 DEBUG = True
 TESTING = False
 
 # CORS and BrowserID configurations
-CORS_CLIENT_DOMAIN = 'dev.naja.cc'
+if 'FLASK_CORS_CLIENT_DOMAIN_DEV' in os.environ:
+    CORS_CLIENT_DOMAIN = os.environ['FLASK_CORS_CLIENT_DOMAIN_DEV']
+else:
+    CORS_CLIENT_DOMAIN = 'dev.naja.cc'
 BROWSERID_CLIENT_DOMAIN = CORS_CLIENT_DOMAIN
 
 # MongoDB options
