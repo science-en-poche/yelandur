de68fb41f99a3c4826cf5f8ad3bbd8df17ad29d4
Sébastien Lerique <sl@mehho.net>
2015-01-12 17:22:36 -0300

    Remove useless runserver command

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 
 import json
 
-from flask.ext.script import Manager, Server
+from flask.ext.script import Manager
 
 from yelandur import create_app
 
@@ -10,15 +10,6 @@ from yelandur import create_app
 manager = Manager(create_app)
 manager.add_option('-m', '--mode', dest='mode', default='dev',
                    required=False)
-
-
-class RunServer(Server):
-
-    def handle(self, app, *args, **kwargs):
-        app.run(host=app.config['HOST'])
-
-
-manager.add_command('runserver', RunServer())
 
 
 @manager.command
