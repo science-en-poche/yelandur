8312f07139dd523d2d0d1be66518ba0eb7c4d178
Sébastien Lerique <sl@mehho.net>
2015-03-10 12:15:39 -0300

    Page results export

--- manage.py
+++ manage.py
@@ -21,10 +21,17 @@ def export_results(*args, **kwargs):
         json.dump({'profiles': Profile.objects.to_jsonable_private()},
                   p, indent=2, separators=(',', ': '))
 
-    print "Exporting results to 'results.json'"
-    with open('results.json', 'w') as r:
-        json.dump({'results': Result.objects.to_jsonable_private()},
-                  r, indent=2, separators=(',', ': '))
+    page_size = 10000
+    results = Result.objects
+    n_results = results.count()
+    pages_indices = range(1 + n_results / page_size)
+    print "Exporting results to 'results-{{{}}}.json'".format(
+        ', '.join(pages_indices))
+    for i in pages_indices:
+        results_range = results[i*page_size:(i+1)*page_size]
+        with open('results-{}.json'.format(i), 'w') as r:
+            json.dump({'results': results_range.to_jsonable_private()},
+                      r, indent=2, separators=(',', ': '))
 
 
 if __name__ == "__main__":
